f6ee4478172306c829491f372abe9a568a634a94
Tanya Raikwar <115107614+Tanya2303@users.noreply.github.com>
2023-02-04 21:59:32 +0530

    Add page

---
+++ client/src/pages/TherapySessions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TherapySessions() {
+    return (
+        <div>
+            <div className='text-center text-6xl font-base text-white'>Therapy Sessions</div>
+        </div>
+    )
+}
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import FoodDisorder from '../pages/FoodDisorder';
 import MentalHealth from '../pages/MentalHealth';
+import TherapySessions from '../pages/TherapySessions';
 import MyProgress from '../pages/MyProgress';
 
 function Index() {
@@ -17,6 +18,7 @@ function Index() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/fooddisorder" element={<FoodDisorder />} />
                 <Route path="/mentalhealth" element={<MentalHealth />} />
+                <Route path="/therapysessions" element={<TherapySessions />} />
                 <Route path="/myprogress" element={<MyProgress />} />
             </Routes>
         </BrowserRouter>
